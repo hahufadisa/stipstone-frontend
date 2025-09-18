c3073d90fcf483360edc5c9f8638fb983b8bcce8
Diyor Safarov <tankisttanjig@gmail.com>
2025-09-18 22:26:27 +0300

    fix: consolelog

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,8 +1,17 @@
 import { AppShell } from "@mantine/core";
 import MobileHeader from "./components/Header/MobileHeader";
 import ContactSection from "./components/Sections/ContactSection";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    console.log(pathname, "pathname");
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <AppShell>
       <MobileHeader />
--- vite.config.ts
+++ vite.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
     },
     host: true
   },
-  esbuild: {
-    drop: ['console', 'debugger']
-  }
+  // esbuild: {
+  //   drop: ['console', 'debugger']
+  // }
 })
